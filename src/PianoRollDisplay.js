836842fc4ec74470b7f44430fa737c79c2d07e3d
Jerzy <118739912+jszufa@users.noreply.github.com>
2023-10-31 12:29:48 +0100

    initial styling

--- src/PianoRollDisplay.js
+++ src/PianoRollDisplay.js
@@ -9,23 +9,9 @@ class PianoRollDisplay extends Component {
   }
 
   componentDidMount() {
-    // Create a new instance of your class and attach it to the <svg> element
     const svgElement = this.svgRef.current;
-    new PianoRoll(svgElement, this.props.partData); // Create a new instance
+    new PianoRoll(svgElement, this.props.partData);
   }
-
-  //???jaki argument tu wstawić?
-
-  //stworzyć cadr divy z odpowiednimi numerami
-  //potrzebuję
-  //1. podzielić dane porcjami po 60 elementów tablicy
-  //2. stworzyć odpowiednie divy i svg z dostępem do tych danych
-  //3. czyli przydałaby mi się budowa pojedynczego pianoRoll (coś jak post)
-
-
-  //pytanie za sto punktów - jak tworzyć odpowiednie instacje piano Roll w Reacie?
-  //Czy muszę przerobić cały ten kod? Moim zdaniem, szkoda zachodu na przerabianie tworzenia svg na logikę funkcyjnych komponentów.
-
 
 
   render() {
